cea4edecb86b243ba742dcf7e019af3508668750
Peter Schneider-Kamp <petersk@imada.sdu.dk>
2026-06-23 15:34:50 +0200

    Revert "Respect TORCHDYNAMO_DISABLE"
    This reverts commit 76b1fff6289e7b99e7e7802c4b142a2a8cac8621.

--- src/synthcity/utils/constants.py
+++ src/synthcity/utils/constants.py
@@ -1,8 +1,4 @@
 # third party
 import torch
 
-from synthcity.utils.torch import disable_torch_dynamo
-
-disable_torch_dynamo()
-
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
--- src/synthcity/version.py
+++ src/synthcity/version.py
@@ -1,4 +1,4 @@
-__version__ = "0.2.17"
+__version__ = "0.2.16"
 
 MAJOR_VERSION = ".".join(__version__.split(".")[:-1])
 PATCH_VERSION = __version__.split(".")[-1]
