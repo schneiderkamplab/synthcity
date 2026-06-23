76b1fff6289e7b99e7e7802c4b142a2a8cac8621
Peter Schneider-Kamp <petersk@imada.sdu.dk>
2026-06-23 15:14:34 +0200

    Respect TORCHDYNAMO_DISABLE

--- src/synthcity/utils/constants.py
+++ src/synthcity/utils/constants.py
@@ -1,4 +1,8 @@
 # third party
 import torch
 
+from synthcity.utils.torch import disable_torch_dynamo
+
+disable_torch_dynamo()
+
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
